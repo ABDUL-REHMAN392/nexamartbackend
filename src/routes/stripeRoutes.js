13c12c11f8bd764c5185f0904a276cb256f28a61
ABDUL-REHMAN392 <abdulrehmanrafique01@gmail.com>
2026-03-10 12:04:06 +0500

    fix: stripe webhook raw body conflict

--- src/routes/stripeRoutes.js
+++ src/routes/stripeRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { stripeWebhook } from "../controllers/stripeController.js";
 
 const router = express.Router();
-router.post("/webhook", express.raw({ type: "application/json" }), stripeWebhook);
+
+router.post("/webhook", stripeWebhook);
 
 export default router;
