90733cd09c9bea31d6fb43ed86d227fc7e30130c
ABDUL-REHMAN392 <abdulrehmanrafique01@gmail.com>
2026-03-26 12:58:06 +0500

    update auth cookies for cross-domain production support

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -15,7 +15,7 @@ export const setAccessTokenCookie = (res, token) => {
   res.cookie("accessToken", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 15 * 60 * 1000, // 15 minutes
   });
 };
@@ -25,7 +25,7 @@ export const setRefreshTokenCookie = (res, token) => {
   res.cookie("refreshToken", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
